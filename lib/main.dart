a2171736ae8c6ceaa201f8c2d6812dc31bb08945
nealsotes <nealfordsotes@gmail.com>
2022-07-27 14:42:48 +0800

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:abangi_v1/pages/welcome.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(Welcome());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(Welcome());
+}
